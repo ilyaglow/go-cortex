59d80ea188e5aaa9348126ad5117cfae1abe3501
Ilya Glotov <ilya.glotov@gmail.com>
2017-11-27 18:27:29 +0300

    Enhance tests, define short ones

--- analyzers_test.go
+++ analyzers_test.go
@@ -1,11 +1,29 @@
 package gocortex
 
 import (
+	"encoding/json"
 	"log"
 	"testing"
+	"time"
 )
 
+var getAnalyzerResponse []byte = []byte(`
+{
+  "name": "File_Info",
+  "version": "1.0",
+  "description": "Parse files in several formats such as OLE and OpenXML to detect VBA macros, extract their source code, generate useful information on PE, PDF files and much more.",
+  "dataTypeList": [
+    "file"
+  ],
+  "id": "File_Info_1_0"
+}
+`)
+
 func TestListAnalyzers(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping ListAnalyzers")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 	a, err := client.ListAnalyzers("*")
 	if err != nil {
@@ -27,6 +45,10 @@ func TestListAnalyzers(t *testing.T) {
 }
 
 func TestGetAnalyzer(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping GetAnalyzer")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 	mm, err := client.GetAnalyzer("MaxMind_GeoIP_3_0")
 	if err != nil {
@@ -38,7 +60,11 @@ func TestGetAnalyzer(t *testing.T) {
 	}
 }
 
-func TestRunAnalyzer(t *testing.T) {
+func TestRunAnalyzerAndGetJob(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping RunAnalyzer")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 
 	j := &JobBody{
@@ -49,13 +75,31 @@ func TestRunAnalyzer(t *testing.T) {
 		},
 	}
 
-	_, err := client.RunAnalyzer("MaxMind_GeoIP_3_0", j)
+	job, err := client.RunAnalyzer("MaxMind_GeoIP_3_0", j)
 	if err != nil {
-		t.Error("Can't run analyzer")
+		t.Errorf("Can't run analyzer: %s", err.Error())
+	}
+
+	if job.Status != "InProgress" {
+		t.Errorf("Job status is not InProgress: %s", job.Status)
+	}
+
+	time.Sleep(10000 * time.Millisecond)
+	sjob, err := client.GetJob(job.ID)
+	if err != nil {
+		t.Errorf("Failed to get a job: %s", err.Error())
+	}
+
+	if sjob.Status != "Success" {
+		t.Errorf("MaxMind analyzer has not succeded: %s", sjob.Status)
 	}
 }
 
 func TestRunAnalyzerThenGetReport(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping RunAnalyzerThenGetReport")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 
 	j := &JobBody{
@@ -73,6 +117,10 @@ func TestRunAnalyzerThenGetReport(t *testing.T) {
 }
 
 func TestAnalyzeData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping AnalyzeData")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 
 	j := &JobBody{
@@ -89,5 +137,44 @@ func TestAnalyzeData(t *testing.T) {
 	}
 	for m := range messages {
 		log.Printf("%s: %s", m.AnalyzerID, m.Status)
+
+		status, err := client.DeleteJob(m.ID)
+		if err != nil {
+			t.Errorf("Can't delete a job: %s", err.Error())
+		}
+
+		if status != true {
+			t.Error("Job has not been deleted")
+		}
+	}
+}
+
+func TestGetAnalyzersResponse(t *testing.T) {
+	a := Analyzer{}
+
+	if err := json.Unmarshal(getAnalyzerResponse, &a); err != nil {
+		t.Error("Can't unmarshal predefined list analyzers response")
+	}
+
+	if a.ID != "File_Info_1_0" {
+		t.Error("Wrong analyzer ID")
+	}
+
+	if a.Name != "File_Info" {
+		t.Error("Wrong analyzer name")
+	}
+
+	if a.Description != "Parse files in several formats such as OLE and OpenXML to detect VBA macros, extract their source code, generate useful information on PE, PDF files and much more." {
+		t.Error("Wrong analyzer description")
+	}
+
+	if a.Version != "1.0" {
+		t.Error("Wrong analyzer version")
+	}
+
+	for _, dt := range a.DataTypeList {
+		if dt != "file" {
+			t.Error("Wrong analyzer datatype")
+		}
 	}
 }
--- jobs_test.go
+++ jobs_test.go
@@ -1,10 +1,137 @@
 package gocortex
 
 import (
+	"encoding/json"
 	"testing"
 )
 
+var listJobsResponse []byte = []byte(`
+[
+  {
+    "id": "OsmbnQJGmeCgvDxP",
+    "analyzerId": "OTXQuery_1_0",
+    "status": "Failure",
+    "date": 1490194495264,
+    "artifact": {
+      "data": "8.8.8.8",
+      "attributes": {
+        "dataType": "ip",
+        "tlp": 2
+      }
+    }
+  },
+  {
+    "id": "c9uZDbHBf32DdIVJ",
+    "analyzerId": "MaxMind_GeoIP_2_0",
+    "status": "Success",
+    "date": 1490194495262,
+    "artifact": {
+      "data": "8.8.8.8",
+      "attributes": {
+        "dataType": "ip",
+        "tlp": 2,
+        "content-type": "text/x-python-script",
+        "filename": "sample.py"
+      }
+    }
+  },
+  {
+    "id": "OcFlZbLNNUsIiJZq",
+    "analyzerId": "HippoMore_1_0",
+    "status": "InProgress",
+    "date": 1490194495259,
+    "artifact": {
+      "data": "8.8.8.8",
+      "attributes": {
+        "dataType": "ip",
+        "tlp": 2
+      }
+    }
+  }
+]
+`)
+
+var analysisJobResponse []byte = []byte(`
+{
+    "id": "ymlrxZB8efyZhFEg",
+    "analyzerId": "Hipposcore_1_0",
+    "status": "Success",
+    "date": 1490263456480,
+    "artifact": {
+        "data": "mydomain.com",
+        "attributes": {
+            "dataType": "domain",
+            "tlp": 2,
+            "content-type": "text/x-python-script",
+            "filename": "sample.py"
+        }
+    }
+}
+`)
+
+var getJobReportResponse []byte = []byte(`
+{
+  "id": "vVQu93ps4PwHOtLv",
+  "analyzerId": "File_Info_1_0",
+  "status": "Success",
+  "date": 1490204071457,
+  "artifact": {
+    "attributes": {
+      "dataType": "file",
+      "tlp": 2,
+      "content-type": "text/x-python-script",
+      "filename": "sample.py"
+    }
+  },
+  "report": {
+    "artifacts": [
+      {
+        "type": "sha1",
+        "value": "cd1c2da4de388a4b5b60601f8b339518fe8fbd31"
+      },
+      {
+        "type": "sha256",
+        "value": "fd1755c7f1f0f85597cf2a1f13f5cbe0782d9e5597aca410da0d5f26cda26b97"
+      },
+      {
+        "type": "md5",
+        "value": "3aa598d1f0d50228d48fe3a792071dde"
+      }
+    ],
+    "full": {
+      "Mimetype": "text/x-python",
+      "Identification": {
+        "ssdeep": "24:8ca1hbLcd8yutXHbLcTtvbrbLcvtEbLcWmtlbLca66/5:8zHbLcdbOXbLc5jrbLcVEbLcPlbLcax",
+        "SHA1": "cd1c2da4de388a4b5b60601f8b339518fe8fbd31",
+        "SHA256": "fd1755c7f1f0f85597cf2a1f13f5cbe0782d9e5597aca410da0d5f26cda26b97",
+        "MD5": "3aa598d1f0d50228d48fe3a792071dde"
+      },
+      "filetype": "python script",
+      "Magic": "Python script, ASCII text executable",
+      "Exif": {
+        "ExifTool:ExifToolVersion": 10.36
+      }
+    },
+    "success": true,
+    "summary": {
+      "taxonomies": [
+        {
+          "predicate": "Predicate",
+          "namespace": "Namespace",
+          "value": "\"Value\"",
+          "level": "info"
+        }
+      ]
+    }
+  }
+}
+`)
+
 func TestListJobs(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping ListJobs")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 
 	_, err := client.ListJobs()
@@ -14,6 +141,10 @@ func TestListJobs(t *testing.T) {
 }
 
 func TestListFilteredJobs(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping ListFilteredJobs")
+	}
+
 	client := NewClient("http://127.0.0.1:9000")
 
 	jf := &JobsFilter{
@@ -24,5 +155,102 @@ func TestListFilteredJobs(t *testing.T) {
 	_, err := client.ListFilteredJobs(jf)
 	if err != nil {
 		t.Errorf("Can't list filtered jobs: %s", err.Error())
+	}
+}
+
+func TestListJobsResponse(t *testing.T) {
+	j := []Job{}
+	if err := json.Unmarshal(listJobsResponse, &j); err != nil {
+		t.Error("Can't unmarshal predefined list of jobs")
+	}
+
+	if len(j) != 3 {
+		t.Error("Wrong number of predefined listed jobs")
+	}
+}
+
+func TestJobBrief(t *testing.T) {
+	j := Job{}
+	if err := json.Unmarshal(analysisJobResponse, &j); err != nil {
+		t.Error("Can't unmarshal predefined analysis job")
+	}
+
+	if j.ID != "ymlrxZB8efyZhFEg" {
+		t.Error("Wrong ID")
+	}
+
+	if j.AnalyzerID != "Hipposcore_1_0" {
+		t.Error("Wrong AnalyzerID")
+	}
+
+	if j.Status != "Success" {
+		t.Error("Wrong status")
+	}
+
+	if j.Date != 1490263456480 {
+		t.Error("Wrong date")
+	}
+
+	if j.Artifact.Data != "mydomain.com" {
+		t.Error("Wrong artifact domain")
+	}
+
+	if j.Artifact.Attributes.DataType != "domain" {
+		t.Error("Wrong attribute datatype")
+	}
+
+	if j.Artifact.Attributes.TLP != 2 {
+		t.Error("Wrong attribute TLP")
+	}
+
+	if j.Artifact.Attributes.ContentType != "text/x-python-script" {
+		t.Error("Wrong attribute content-type")
+	}
+
+	if j.Artifact.Attributes.Filename != "sample.py" {
+		t.Error("Wrong attribute filename")
+	}
+}
+
+func TestJobReport(t *testing.T) {
+	j := JobReport{}
+
+	if err := json.Unmarshal(getJobReportResponse, &j); err != nil {
+		t.Error("Can't unmarshal predefined job report")
+	}
+
+	if j.Report.Success != true {
+		t.Error("Wrong report success status")
+	}
+
+	for _, tx := range j.Taxonomies() {
+		if tx.Predicate != "Predicate" {
+			t.Error("Wrong taxonomy predicate")
+		}
+
+		if tx.Namespace != "Namespace" {
+			t.Error("Wrong taxonomy namespace")
+		}
+
+		if tx.Value != `"Value"` {
+			t.Error("Wrong taxonomy value")
+		}
+
+		if tx.Level != "info" {
+			t.Error("Wrong taxonomy level")
+		}
+	}
+
+	for i, af := range j.Report.Artifacts {
+		switch i {
+		case 0:
+			if af.AType != "sha1" {
+				t.Error("Wrong artifact type")
+			}
+
+			if af.Value != "cd1c2da4de388a4b5b60601f8b339518fe8fbd31" {
+				t.Error("Wrong artifact value")
+			}
+		}
 	}
 }
