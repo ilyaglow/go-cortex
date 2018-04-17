f6af9ec11614bf1b98a52e8d690220f8b03a4879
Ilya Glotov <ilya.glotov@gmail.com>
2018-04-17 20:10:02 +0300

    Add cortexutil

---
+++ util.go
@@ -0,0 +1,210 @@
+package cortex
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/asaskevich/govalidator"
+)
+
+var (
+	domain = `([a-zA-Z0-9_]{1}[a-zA-Z0-9_-]{0,62}){1}(\.[a-zA-Z]{1}[a-zA-Z]{0,62})+[\._]?`
+	// inspired by govalidator.CreditCard
+	rxCC     = regexp.MustCompile("(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})")
+	rxDomain = regexp.MustCompile(domain)
+	// inspired by govalidator.Email
+	rxEmail          = regexp.MustCompile("(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@" + domain)
+	rxHash           = regexp.MustCompile(`([0-9a-fA-F]{64}|[0-9a-fA-F]{40}|[0-9a-fA-F]{32})`)
+	rxIPv4           = regexp.MustCompile(`((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.)?){1}`)
+	rxIPv6           = regexp.MustCompile(govalidator.IP)
+	rxRegistryKey    = regexp.MustCompile(`(HKEY|HKLM|HKCU|HKCR|HKCC)[\\a-zA-Z0-9]+`)
+	rxURL            = regexp.MustCompile(govalidator.URLSchema + govalidator.URLUsername + `?` + `((` + govalidator.URLIP + `|(\[` + govalidator.IP + `\])|(([a-zA-Z0-9]([a-zA-Z0-9-_]+)?[a-zA-Z0-9]([-\.][a-zA-Z0-9]+)*)|(` + govalidator.URLSubdomain + `?))?(([a-zA-Z\x{00a1}-\x{ffff}0-9]+-?-?)*[a-zA-Z\x{00a1}-\x{ffff}0-9]+)(?:\.([a-zA-Z\x{00a1}-\x{ffff}]{1,}))?))\.?` + govalidator.URLPort + `?` + govalidator.URLPath + `?`)
+	rxUserAgent      = regexp.MustCompile(`(Mozilla/[45]\.0 |AppleWebKit/[0-9]{3}\.[0-9]{2} |Chrome/[0-9]{2}\.[0-9]\.[0-9]{4}\.[0-9]{3} |Safari/[0-9]{3}\.[0-9]{2} ).*`)
+	rxBitcoinAddress = regexp.MustCompile(`[13][a-km-zA-HJ-NP-Z1-9]{25,34}`)
+)
+
+// Rxs represents map of regexes
+var Rxs = map[string]*regexp.Regexp{
+	"cc":           rxCC,
+	"ipv4":         rxIPv4,
+	"ipv6":         rxIPv6,
+	"domain":       rxDomain,
+	"email":        rxEmail,
+	"hash":         rxHash,
+	"registry":     rxRegistryKey,
+	"url":          rxURL,
+	"user-agent":   rxUserAgent,
+	"bitcoin-addr": rxBitcoinAddress,
+}
+
+// cfg represents custom config field in the Analyzer definition
+type cfg map[string]interface{}
+
+// Error returns unsuccessful Report with an error message
+func (j *JobInput) Error(msg string) {
+	r := &ReportBody{
+		Success:      false,
+		ErrorMessage: msg,
+		Input:        *j,
+	}
+
+	body, err := json.Marshal(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(body))
+	os.Exit(1)
+}
+
+// Report constructs Cortex Report by raw body and taxonomies
+func (j *JobInput) Report(body interface{}, taxs []Taxonomy) {
+	mb, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := &ReportBody{
+		Success:    true,
+		Input:      *j,
+		Artifacts:  ExtractArtifacts(string(mb)),
+		FullReport: body,
+		Summary:    Summary{taxs},
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(b))
+	os.Exit(0)
+}
+
+func (j *JobInput) allowedTLP() bool {
+	// if maxtlp is not set, make it to maximum
+	maxtlp, err := j.Config.GetFloat("max_tlp")
+	if err != nil {
+		maxtlp = 3
+	}
+
+	if j.TLP > int(maxtlp) {
+		return false
+	}
+	return true
+}
+
+// ExtractArtifacts extracts all artifacts from report string
+func ExtractArtifacts(body string) []Artifact {
+	var ars []Artifact
+	ma := make(map[string]bool)
+	for t, r := range Rxs {
+		res := r.FindAllString(body, -1)
+		if res == nil {
+			continue
+		}
+
+		for i := range res {
+			if _, ok := ma[res[i]]; ok {
+				continue
+			}
+
+			ma[res[i]] = true
+			ars = append(ars, Artifact{
+				Data: res[i],
+				Attributes: ArtifactAttributes{
+					DataType: t,
+				},
+			})
+		}
+	}
+	return ars
+}
+
+// GetString is a getter for string type
+func (c cfg) GetString(key string) (string, error) {
+	var res string
+	var err error
+
+	switch c[key].(type) {
+	case string:
+		res = c[key].(string)
+	default:
+		res = ""
+		err = fmt.Errorf("Wrong type chosen (%T)", c[key])
+	}
+
+	return res, err
+}
+
+// GetFloat is a getter for float64 type
+func (c cfg) GetFloat(key string) (float64, error) {
+	var res float64
+	var err error
+
+	switch c[key].(type) {
+	case float64:
+		res = c[key].(float64)
+	default:
+		res = 0
+		err = fmt.Errorf("Wrong type chosen (%T)", c[key])
+	}
+
+	return res, err
+}
+
+// GetBool is a getter for bool type
+func (c cfg) GetBool(key string) (bool, error) {
+	var res bool
+	var err error
+
+	switch c[key].(type) {
+	case bool:
+		res = c[key].(bool)
+	default:
+		res = false
+		err = fmt.Errorf("Wrong type chosen (%T)", c[key])
+	}
+	return res, err
+}
+
+// NewInput grabs stdin and bootstraps cortex.JobInput
+func NewInput() (*JobInput, error) {
+	in, err := parseInput(os.Stdin)
+	if err != nil {
+		return nil, err
+	}
+
+	v, err := in.Config.GetBool("check_tlp")
+	if err != nil {
+		return nil, err
+	}
+
+	if v && !in.allowedTLP() {
+		in.Error("TLP is higher than allowed")
+		return nil, nil
+	}
+
+	return in, nil
+}
+
+func parseInput(f io.Reader) (*JobInput, error) {
+	var j JobInput
+	dec := json.NewDecoder(f)
+
+	for {
+		err := dec.Decode(&j)
+		if err == io.EOF {
+			break
+		}
+		if err != nil && err != io.EOF {
+			log.Println(err)
+			return nil, err
+		}
+	}
+
+	return &j, nil
+}
---
+++ util_test.go
@@ -0,0 +1,93 @@
+package cortex
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+var sampleConfig = []byte(`
+{
+    "data": "d41d8cd98f00b204e9800998ecf8427e",
+    "dataType": "hash",
+    "tlp": 0,
+    "config": {
+        "key": "1234567890abcdef",
+        "max_tlp": 3,
+        "check_tlp": true,
+        "service": "GetReport"
+    },
+    "proxy": {
+        "http": "http://myproxy:8080",
+        "https": "https://myproxy:8080"
+    }
+}
+`)
+
+var sampleReader = bytes.NewReader(sampleConfig)
+
+func TestGetters(t *testing.T) {
+	ai, err := parseInput(sampleReader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s, err := ai.Config.GetString("service")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if s != "GetReport" {
+		t.Fatalf("need GetReport, got %s", s)
+	}
+
+	f, err := ai.Config.GetFloat("max_tlp")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if f != 3 {
+		t.Fatalf("need 3, got %f", f)
+	}
+
+	b, err := ai.Config.GetBool("check_tlp")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if b != true {
+		t.Fatalf("need true, got %t", b)
+	}
+}
+
+func TestExtractArtifacts(t *testing.T) {
+	var patternsTest = []struct {
+		report    string
+		typedData map[string][]string
+	}{
+		{`{"report":{"ip":"8.8.8.8"}}`, map[string][]string{"ipv4": []string{"8.8.8.8"}}},
+		{`{"report":{"domain":"test.com"}}`, map[string][]string{"domain": []string{"test.com"}}},
+		{`{"report":{"email":"name@domainname.com"}}`, map[string][]string{"email": []string{"name@domainname.com"}, "domain": []string{"domainname.com"}}},
+		{`{"report":{"url":"https://testdomain.com/handler?parameter=value`, map[string][]string{"domain": []string{"testdomain.com"}, "url": []string{"https://testdomain.com/handler?parameter=value"}}},
+		{`{"report":{"hash1":"ba1f2511fc30423bdbb183fe33f3dd0f", "hash2":"a8fdc205a9f19cc1c7507a60c4f01b13d11d7fd0", "hash3": "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b"}}`, map[string][]string{"hash": []string{"ba1f2511fc30423bdbb183fe33f3dd0f", "a8fdc205a9f19cc1c7507a60c4f01b13d11d7fd0", "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b"}}},
+	}
+
+	for _, p := range patternsTest {
+		as := ExtractArtifacts(p.report)
+		am := artifactsToMap(as)
+
+		if !reflect.DeepEqual(p.typedData, am) {
+			t.Fatalf("need %v, got %v", p.typedData, am)
+		}
+	}
+}
+
+func artifactsToMap(as []Artifact) map[string][]string {
+	m := make(map[string][]string)
+
+	for i := range as {
+		m[as[i].Attributes.DataType] = append(m[as[i].Attributes.DataType], as[i].Data)
+	}
+
+	return m
+}
