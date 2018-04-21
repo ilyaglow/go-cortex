bff9a006d723d40ffe2bb1343f0a8d16cc4f5fa5
Ilya Glotov <ilya.glotov@gmail.com>
2018-04-22 00:01:35 +0300

    Add tests for remaining data types

--- util_test.go
+++ util_test.go
@@ -70,6 +70,10 @@ func TestExtractArtifacts(t *testing.T) {
 		{`{"report":{"email":"name@domainname.com"}}`, map[string][]string{"email": []string{"name@domainname.com"}, "domain": []string{"domainname.com"}}},
 		{`{"report":{"url":"https://testdomain.com/handler?parameter=value`, map[string][]string{"domain": []string{"testdomain.com"}, "url": []string{"https://testdomain.com/handler?parameter=value"}}},
 		{`{"report":{"hash1":"ba1f2511fc30423bdbb183fe33f3dd0f", "hash2":"a8fdc205a9f19cc1c7507a60c4f01b13d11d7fd0", "hash3": "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b"}}`, map[string][]string{"hash": []string{"ba1f2511fc30423bdbb183fe33f3dd0f", "a8fdc205a9f19cc1c7507a60c4f01b13d11d7fd0", "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b"}}},
+		{`{"report":{"ipv6":"2a00:1450:4011:809::1002"}}`, map[string][]string{"ipv6": []string{"2a00:1450:4011:809::1002"}}},
+		{`{"report":{"useragent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"}}`, map[string][]string{"user-agent": []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"}}},
+		{`{"report":{"bitcoinaddr":"12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw"}}`, map[string][]string{"bitcoin-address": []string{"12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw"}}},
+		{`{"report":{"ccnum":"38520000023237"}}`, map[string][]string{"cc": []string{"38520000023237"}}},
 	}
 
 	for _, p := range patternsTest {
