21cef33f52d9eeda91a19c81847a3547dcd719ad
Ilya Glotov <ilya.glotov@gmail.com>
2018-05-25 18:52:31 +0300

    Add configuration field

--- analyzer.go
+++ analyzer.go
@@ -14,23 +14,24 @@ const (
 
 // Analyzer defines a specific Cortex Analyzer
 type Analyzer struct {
-	Author       string      `json:"author"`
-	BaseConfig   string      `json:"baseConfig"`
-	CreatedAt    int64       `json:"createdAt"`
-	CreatedBy    string      `json:"createdBy"`
-	DataTypeList []string    `json:"dataTypeList"`
-	DefinitionID string      `json:"analyzerDefinitionId"`
-	Description  string      `json:"description"`
-	ID           string      `json:"id"`
-	JobCache     interface{} `json:"jobCache,omitempty"` // unknown
-	License      string      `json:"license"`
-	Name         string      `json:"name"`
-	Rate         int         `json:"rate,omitempty"`
-	RateUnit     string      `json:"rateUnit,omitempty"`
-	URL          string      `json:"url"`
-	UpdatedAt    int64       `json:"updatedAt,omitempty"`
-	UpdatedBy    string      `json:"updatedBy,omitempty"`
-	Version      string      `json:"version"`
+	Author        string                 `json:"author"`
+	BaseConfig    string                 `json:"baseConfig"`
+	Configuration map[string]interface{} `json:"configuration"`
+	CreatedAt     int64                  `json:"createdAt"`
+	CreatedBy     string                 `json:"createdBy"`
+	DataTypeList  []string               `json:"dataTypeList"`
+	DefinitionID  string                 `json:"analyzerDefinitionId"`
+	Description   string                 `json:"description"`
+	ID            string                 `json:"id"`
+	JobCache      interface{}            `json:"jobCache,omitempty"` // unknown
+	License       string                 `json:"license"`
+	Name          string                 `json:"name"`
+	Rate          int                    `json:"rate,omitempty"`
+	RateUnit      string                 `json:"rateUnit,omitempty"`
+	URL           string                 `json:"url"`
+	UpdatedAt     int64                  `json:"updatedAt,omitempty"`
+	UpdatedBy     string                 `json:"updatedBy,omitempty"`
+	Version       string                 `json:"version"`
 }
 
 // AnalyzerService is an interface for managing analyzers
